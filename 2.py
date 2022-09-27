436ec73d56512e37c32a4457f6e4289d205a8eee
Alex Custódio <alex.scustod@gmail.com>
2022-09-27 20:02:11 +0000

    atualizando formataçã

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
-#Faça um programa que leia 3 números inteiros e os imprima em ordem crescente.
+# Faça um programa que leia 3 números inteiros e os imprima em ordem crescente.
 
 n1 = int(input())
 n2 = int(input())
 n3 = int(input())
 numeros = []
-numeros.extend([n1,n2,n3])
+numeros.extend([n1, n2, n3])
 numeros.sort()
 for num in numeros:
     print(num)
--- 3891.py
+++ 3891.py
@@ -1,9 +1,9 @@
-#Ler 3 notas e tirar a média
+# Ler 3 notas e tirar a média
 n1 = float(input())
 n2 = float(input())
 n3 = float(input())
 
-mediaNormal = (n1 + n2 + n3) /3
+mediaNormal = (n1 + n2 + n3) / 3
 mediaPonderada1 = (n1 * 2 + n2 * 2 + n3 * 3) / (2 + 2 + 3)
 mediaPonderada2 = (n1 * 1 + n2 * 2 + n3 * 2) / (1 + 2 + 2)
 print(f"{mediaNormal:.2f}")
